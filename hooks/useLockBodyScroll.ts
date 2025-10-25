8abc97f0c86a918f812846a7a1b5ba1c6f62c6ee
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-10-25 19:55:58 +0900

    Feat: 스크롤 락 훅 제작

---
+++ hooks/useLockBodyScroll.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function useLockBodyScroll(lock: boolean) {
+  useEffect(() => {
+    if (!lock) return;
+    const original = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = original;
+    };
+  }, [lock]);
+}
