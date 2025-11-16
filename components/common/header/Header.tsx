35fab640908b0fa2815050c7b1919d1a38f0785e
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-11-16 16:45:34 +0900

    Design: 헤더 폭 수정(본문 폭과 동일)

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -8,25 +8,27 @@ export default function Header() {
   return (
     <>
       <header className="header">
-        {/* 좌측 - 홈버튼 */}
-        <Link
-          href="/"
-          className="font-semibold text-lg"
-          onClick={(e) => {
-            // url 해시 제거
-            window.history.replaceState(null, "", "/");
-            // section 상태 동기화
-            window.dispatchEvent(new HashChangeEvent("hashchange"));
-          }}
-        >
-          {/* TODO 추후 수정 */}
-          HOME
-        </Link>
+        <div className="header-inner">
+          {/* 좌측 - 홈버튼 */}
+          <Link
+            href="/"
+            className="font-semibold text-lg"
+            onClick={(e) => {
+              // url 해시 제거
+              window.history.replaceState(null, "", "/");
+              // section 상태 동기화
+              window.dispatchEvent(new HashChangeEvent("hashchange"));
+            }}
+          >
+            {/* TODO 추후 수정 */}
+            HOME
+          </Link>
 
-        {/* 우측 - nav, 다크모드 버튼 */}
-        <div className="flex items-center gap-4 md:gap-6">
-          <Nav />
-          <ThemeToggle />
+          {/* 우측 - nav, 다크모드 버튼 */}
+          <div className="flex items-center gap-4 md:gap-6">
+            <Nav />
+            <ThemeToggle />
+          </div>
         </div>
       </header>
     </>
