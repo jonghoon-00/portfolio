d556263ec7bc161474d1d4e5c068c07c801fccf9
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-11-19 21:48:17 +0900

    Fix: tailwind 다크모드 import 현 버전에 맞게 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class", '[data-theme="dark"]'],
+  // darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
