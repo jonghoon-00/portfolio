5d8c7fbc019585aead21eeaa0cb1dec2cdbc05e2
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-12-28 20:20:16 +0900

    Feat: cloudinary 웹 url 상수 객체 생성

---
+++ constants/cloudinary.ts
@@ -0,0 +1,9 @@
+// 자동 최적화 URL 형식 변환을 위해 'image/upload/' 이후 이미지 경로만 사용
+export const CLOUDINARY = {
+  K_NOSTALGIA: {
+    COUPON_FLOW: "v1763460174/%EC%BF%A0%ED%8F%B0_pdwodh.gif",
+    ADDRESS_FLOW: "v1766904862/address-flow_sawaq0.gif",
+    MOBILE_TOOLTIP: "v1766904862/tooltip-gif_zwhqgu.gif",
+    POPSTATE_CONTROL: "v1766904861/popstate-control_hfcnt8.gif",
+  },
+} as const;
